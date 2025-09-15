42429177376d42cb72c78e37101fc1195488db30
Jenna Worthen <worthenj.281@gmail.com>
2025-09-15 11:37:51 -0600

    Part 2 with feature engineering

--- BikeShare.R
+++ BikeShare.R
@@ -1,2 +1,94 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(dplyr)
+library(DataExplorer)
+library(patchwork)
+library(lubridate)  # for hour extraction
+
+# Load data
+train <- vroom("C:/Users/Jenna/OneDrive/Desktop/Statistics/Stat 348/BikeShare/train.csv")
+test <- vroom("C:/Users/Jenna/OneDrive/Desktop/Statistics/Stat 348/BikeShare/test.csv")
+
+# Old factor conversions (commented out)
+# train$weather <- as.factor(train$weather)
+# train$season <- as.factor(train$season)
+# train$holiday <- as.factor(train$holiday)
+# train$workingday <- as.factor(train$workingday)
+
+# Old exploratory plots (commented out)
+# glimpse(train)
+# DataExplorer::plot_correlation(train)
+# DataExplorer::plot_intro(train)
+# DataExplorer::plot_bar(train)
+# DataExplorer::plot_missing(train)
+# GGally::ggpairs(train)
+# DataExplorer::plot_histogram(train)
+
+# Old visualizations (commented out)
+# p1 <- ggplot(train, aes(x=humidity)) + geom_histogram(bins=30, fill="skyblue")
+# p2 <- ggplot(data= train)+ geom_bar(aes(x=weather, fill=season))
+# p3 <- ggplot(data=train)+geom_point(aes(x=atemp, y=temp))
+# p4 <- ggplot(data=train)+geom_point(aes(x=casual, y=count))
+# (p1 + p2) / (p3 + p4)
+
+# HW Step 1: Data Cleaning
+train_clean <- train %>%
+  select(-casual, -registered) %>% # Remove columns as per HW
+  mutate(count = log(count))        # Log-transform target variable
+
+# HW Step 2: Feature Engineering
+bike_recipe <- recipe(count ~ ., data = train_clean) %>%
+  step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% # Recode weather 4->3
+  step_mutate(weather = factor(weather)) %>%
+  step_mutate(holiday = factor(holiday)) %>%
+  step_mutate(workingday = factor(workingday)) %>%
+  step_mutate(season = factor(season)) %>%  
+  # step_mutate(hour = hour(datetime)) %>%
+  step_time(datetime, features="hour") %>%
+# Convert season to factor
+  step_zv(all_predictors()) # Remove zero-variance predictors
+
+# Prepare and bake recipe (optional check)
+prepped_recipe <- prep(bike_recipe)
+baked_data <- bake(prepped_recipe, new_data = train_clean)
+head(baked_data, 5)  # Show first 5 rows for HW
+
+# HW Step 3: Linear Regression Workflow
+bike_workflow <- workflow() %>%
+  add_recipe(bike_recipe) %>%
+  add_model(
+    linear_reg() %>% 
+      set_engine("lm") %>% 
+      set_mode("regression")
+  )
+
+bike_fit <- bike_workflow %>% fit(data = train_clean)
+
+# HW Step 4: Predict on Test
+bike_preds <- predict(bike_fit, new_data = test) %>%
+  mutate(count = exp(.pred))  # Back-transform log(count)
+
+kaggle_submission <- bike_preds %>%
+  bind_cols(test %>% select(datetime)) %>%  # Keep original test datetime
+  select(datetime, count) %>%               # Column order
+  mutate(count = pmax(0, count)) %>%
+  mutate(datetime=as.character(format(datetime)))
+
+vroom_write(kaggle_submission, "LinearPreds_v2.csv", delim = ",")
+
+
+# Old linear model approach (commented out)
+# my_linear_model <- linear_reg() |> 
+#   set_engine("lm") |>
+#   set_mode("regression") |>
+#   fit(formula=count ~. -datetime , data = newtrain)
+# bike_predictions <- predict(my_linear_model, new_data=test)
+# bike_predictions
+# kaggle_submission <- bike_predictions %>%
+#   bind_cols(., test) %>% 
+#   select(datetime, .pred) %>%
+#   rename(count=.pred) %>%
+#   mutate(count=pmax(0, count)) %>%
+#   mutate(datetime=as.character(format(datetime)))
+# vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim = ",")
