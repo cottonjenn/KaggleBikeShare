1df64d07eb380316dfd748f5aa9ca0fe1e8a6b2c
Jenna Worthen <worthenj.281@gmail.com>
2025-09-16 08:25:32 -0600

    penalized regression

--- BikeShare.R
+++ BikeShare.R
@@ -40,34 +40,38 @@ train_clean <- train %>%
 # HW Step 2: Feature Engineering
 bike_recipe <- recipe(count ~ ., data = train_clean) %>%
   step_mutate(weather = ifelse(weather == 4, 3, weather)) %>% # Recode weather 4->3
-  step_mutate(weather = factor(weather)) %>%
-  step_mutate(holiday = factor(holiday)) %>%
-  step_mutate(workingday = factor(workingday)) %>%
-  step_mutate(season = factor(season)) %>%  
+  # step_mutate(weather = factor(weather)) %>%
+  # step_mutate(holiday = factor(holiday)) %>%
+  # step_mutate(workingday = factor(workingday)) %>%
+  # step_mutate(season = factor(season)) %>%  
   # step_mutate(hour = hour(datetime)) %>%
   step_time(datetime, features="hour") %>%
-# Convert season to factor
+  step_date(datetime, features="month") %>%
+  step_date(datetime, features = "doy") %>%
+  step_rm(datetime) %>%
+  step_normalize(all_numeric_predictors()) %>%
+  step_dummy(all_nominal_predictors()) %>%
   step_zv(all_predictors()) # Remove zero-variance predictors
 
 # Prepare and bake recipe (optional check)
-prepped_recipe <- prep(bike_recipe)
-baked_data <- bake(prepped_recipe, new_data = train_clean)
-head(baked_data, 5)  # Show first 5 rows for HW
+# prepped_recipe <- prep(bike_recipe)
+# baked_data <- bake(prepped_recipe, new_data = train_clean)
+# head(baked_data, 5)  # Show first 5 rows for HW
 
-# HW Step 3: Linear Regression Workflow
-bike_workflow <- workflow() %>%
+# first penalty=0.01, mixture=0.01
+# second penalty=0.5, mixture=0.05
+# 3 penalty=0.1, mixture=0.1
+# 4 penalty=0.5, mixture=0.1
+
+## new penalized regression ----------------------------------
+preg_model <- linear_reg(penalty=0.1, mixture=0.1) %>%
+  set_engine("glmnet") # Function to fit in R11
+preg_wf <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(
-    linear_reg() %>% 
-      set_engine("lm") %>% 
-      set_mode("regression")
-  )
-
-bike_fit <- bike_workflow %>% fit(data = train_clean)
-
-# HW Step 4: Predict on Test
-bike_preds <- predict(bike_fit, new_data = test) %>%
-  mutate(count = exp(.pred))  # Back-transform log(count)
+  add_model(preg_model) %>%
+  fit(data=train_clean)
+bike_preds <- predict(preg_wf, new_data=test) %>%
+  mutate(count = exp(.pred))
 
 kaggle_submission <- bike_preds %>%
   bind_cols(test %>% select(datetime)) %>%  # Keep original test datetime
@@ -78,7 +82,30 @@ kaggle_submission <- bike_preds %>%
 vroom_write(kaggle_submission, "LinearPreds_v2.csv", delim = ",")
 
 
-# Old linear model approach (commented out)
+## # HW Step 3: Linear Regression Workflow ------------------
+# bike_workflow <- workflow() %>%
+#   add_recipe(bike_recipe) %>%
+#   add_model(
+#     linear_reg() %>% 
+#       set_engine("lm") %>% 
+#       set_mode("regression")
+#   )
+# 
+# bike_fit <- bike_workflow %>% fit(data = train_clean)
+
+# # HW Step 4: Predict on Test
+# bike_preds <- predict(bike_fit, new_data = test) %>%
+#   mutate(count = exp(.pred))  # Back-transform log(count)
+
+# kaggle_submission <- bike_preds %>%
+#   bind_cols(test %>% select(datetime)) %>%  # Keep original test datetime
+#   select(datetime, count) %>%               # Column order
+#   mutate(count = pmax(0, count)) %>%
+#   mutate(datetime=as.character(format(datetime)))
+# 
+# vroom_write(kaggle_submission, "LinearPreds_v2.csv", delim = ",")
+
+########## Old linear model approach (commented out)
 # my_linear_model <- linear_reg() |> 
 #   set_engine("lm") |>
 #   set_mode("regression") |>
